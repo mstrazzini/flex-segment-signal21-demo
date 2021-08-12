3518a7ded9527b4d7b3d94171073717625c18edc
Marcos Trazzini <mtrazzini@twilio.com>
2021-08-12 18:03:34 -0300

    added card-declined function

--- serverless/src/functions/card-declined.ts
+++ serverless/src/functions/card-declined.ts
@@ -5,10 +5,15 @@ import {
   ServerlessFunctionSignature,
 } from '@twilio-labs/serverless-runtime-types/types'
 import Analytics from 'analytics-node'
+import axios from 'axios'
 
 type CustomContext = {
   SEGMENT_WRITE_KEY?: string,
-  DEMO_CUSTOMER_PHONE?: string
+  PROFILE_API_SPACE_ID?: string,
+  PROFILE_API_TOKEN?: string,
+  DEMO_CUSTOMER_PHONE?: string,
+  CALL_CENTER_NUMBER?: string,
+  SMS_FROM_NUMBER?: string
 }
 
 type EventPayload = {
@@ -21,14 +26,16 @@ type EventPayload = {
   notifyBy?: 'SMS' | 'PHONE_CALL' | 'WHATSAPP' | 'APP_PUSH'
 }
 
-export const handler: ServerlessFunctionSignature = (
+export const handler: ServerlessFunctionSignature = async (
   context: Context<CustomContext>,
   event: EventPayload,
   callback: ServerlessCallback
 ) => {
-  const { SEGMENT_WRITE_KEY, DEMO_CUSTOMER_PHONE } = context
+  const { SEGMENT_WRITE_KEY, DEMO_CUSTOMER_PHONE, PROFILE_API_SPACE_ID, PROFILE_API_TOKEN } = context
   const analytics = new Analytics(SEGMENT_WRITE_KEY as string)
   const userId = event.userId as string
+  const { reason, amount, city, merchantName, cardBlocked, notifyBy } = event
+  let customerData
   const traitUpdates = { 
     userId,
     traits: {
@@ -49,13 +56,65 @@ export const handler: ServerlessFunctionSignature = (
       ]
     }
   }
+
+  // Get customer data/traits, so we can use other traits (like email) to
+  // trigger the CARD_BLOCKED event
   
+  const url = `https://profiles.segment.com/v1/spaces/${PROFILE_API_SPACE_ID}/collections/users/profiles/user_id:${userId}/traits`
+
+  try {
+    const result = await axios.get(url, {
+      auth: {
+        username: PROFILE_API_TOKEN as string,
+        password: ''
+      }
+    })
+
+    if (result.data && result.data.traits) {
+      customerData = result.data.traits
+    }
+  } catch (error) {
+    console.error(error)
+    callback(error)
+  }
+  
+  // Trigger CARD_DECLINED event
+  analytics.track({
+    userId,
+    event: 'CARD_DECLINED',
+    properties: { reason, amount, city, merchantName, cardBlocked, notifyBy }
+  })
+
+  // Update user traits
   analytics.identify(traitUpdates)
-  
+
   setTimeout(() => {
+    // Flush all pending data to Segment
     analytics.flush((err: Error, data: any) => {
       if (err) callback(err)
-      callback(null, data || traitUpdates)
+
+      // Notify user (optional)
+      if (event.notifyBy === 'SMS' && event.cardBlocked) {
+        const { CALL_CENTER_NUMBER } = context
+
+        context.getTwilioClient().messages
+          .create({
+            from: context.SMS_FROM_NUMBER,
+            to: DEMO_CUSTOMER_PHONE as string, // TODO: get number from user traits
+            body: `Your card has been suspended by fraud prevention. Please contact us as ${CALL_CENTER_NUMBER}`
+          })
+          .then(result => {
+            console.info(`SMS notification sent sucessfully (${result.sid})`)
+            callback(null, data || traitUpdates)
+          })
+          .catch(err => {
+            console.error(err)
+            callback(err)
+          })
+      } else {
+        callback(null, data || traitUpdates)
+      }
     })
   }, 2000)
+  
 }
